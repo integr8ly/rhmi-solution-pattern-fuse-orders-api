bb4599092c7028b6c7b4d59afa8e174f3f21e558
Evan Shortiss <evanshortiss@gmail.com>
2019-10-09 14:53:21 -0700

    fix: issues in template chore: split scripts for docker build and push

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM openjdk:8-jdk-alpine
 
+EXPOSE 8080
+
+USER 1001
+
 COPY target/orders-internal-1.0-SNAPSHOT.jar application.jar
 
 CMD java -jar /application.jar
--- scripts/build-and-push.sh
+++
@@ -1,6 +0,0 @@
-FOO=${QUAY_USER:-evanshortiss}
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path/.."
-docker build . -t rhmi-lab-internal-fuse-api
-docker tag rhmi-lab-internal-fuse-api quay.io/evanshortiss/rhmi-lab-internal-fuse-api:latest
-docker push quay.io/evanshortiss/rhmi-lab-internal-fuse-api:latest
